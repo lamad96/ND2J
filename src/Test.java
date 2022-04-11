d294af2519e27036e140eedb218d8f11a5fe1f86
lamad96 <lamad96@example.com>
2022-04-11 15:33:30 +0200

    MY COMMIT

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		int nb=9;
+		int j=10;
+		
+		do
+		{
+			nb=nb-4;
+		}while(nb>=1);
+		 
+		 System.out.print(nb);
+		 System.out.print(j);
+	}
+
+}
